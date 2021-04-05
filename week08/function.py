d84d2b23fff098ac5c5b9027ad7fb6077d52dc2d
suseh <g00398260@gmit.ie>
2021-04-05 17:42:18 +0100

    attempting to push code to main branch instead of master

--- week08/function.py
+++ week08/function.py
@@ -1,6 +1,11 @@
+#Histogram plots
+#author: Susan Henry
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 xpoints = np.array(range(1,101))
 ypoints = xpoints * xpoints #multiply each entry by itself
+
 plt.plot(xpoints, ypoints)
 plt.show()
---
+++ week09/useFib.py
@@ -0,0 +1,6 @@
+# This program prompts the user for a number and
+# Prints out the fibonacci sequence of that many numbers
+import myFunctions
+
+nTimes = int(input('how many:'))
+print(myFunctions.fibonacci(nTimes))
